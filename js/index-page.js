15a664a74c7d36caf76db25dceb2ab8b392dce59
liyi2013 <285581618@qq.com>
2025-06-01 03:36:10 +0800

    Update index-page.js

--- js/index-page.js
+++ js/index-page.js
@@ -45,7 +45,7 @@ document.addEventListener('DOMContentLoaded', function() {
                 try {
                     window.history.replaceState(
                         { search: keyword }, 
-                        `搜索: ${keyword} - LibreTV`, 
+                        `搜索: ${keyword} - LeoTV`, 
                         window.location.href
                     );
                 } catch (e) {
